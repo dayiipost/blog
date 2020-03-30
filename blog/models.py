5db20be62314b52705326c2a0ad30f516af37fbd
dayii <dainapostizzi@gmail.com>
2020-03-30 19:15:00 -0300

    Templates modificados para mostrar post desde base de datos.

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models 
 from django.utils import timezone
 
+
 class Post(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	title = models.CharField(max_length=200)
@@ -8,7 +9,7 @@ class Post(models.Model):
 	created_date = models.DateTimeField(
 			default=timezone.now)
 	published_date = models.DateTimeField(
-		blank=True, null=True)
+			blank=True, null=True)
 
 	def publish(self):
 		self.published_date = timezone.now()
@@ -17,16 +18,18 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
-# class Comment(models.Model):
-# 	post = models.ForeignKey('post', related_name='comments')
-# 	author = modelsCharField(max_length=200)
-# 	text = models.TexField()
-# 	created_date = models.DateTimeField(default=timezonenow)
-# 	approved_comment = modelsBooleanField(default=False)
+class Comment(models.Model):
+ 	post = models.ForeignKey('post', on_delete=models.CASCADE, related_name='comments')
+ 	author = models.CharField(max_length=200)
+ 	text = models.TextField()
+ 	created_date = models.DateTimeField(default=timezone.now)
+ 	approved_comment = models.BooleanField(default=False)
 
-	# def approve(self):
-	# 	self.approved_comment = True
-	# 	self.save('enter code here')
+ 	def __str__(self):
+ 		return self.text
 
-	# def __str__(self):
-	#    return self.text	
+# def approve(self):
+# 	self.approved_comment = True
+# 	self.save('enter code here')
+
+	
